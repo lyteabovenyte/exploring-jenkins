2e7ee3c8ea21f8b7fc3c1ac7f430d243c563ee2e
lyteabovenyte <lyteabovenyte@gmail.com>
2024-07-18 02:05:58 +0330

    add jenkins master's load balancer DNS URL to the output

--- tf/outputs.tf
+++ tf/outputs.tf
@@ -1,3 +1,8 @@
 output "bastion_public_ip" {
     value = "${aws_instance.bastion.public_ip}"
 }
+
+output "jenkins-master-elb" {
+    description = "load balancer DNS URL"
+    value = aws_elb.jenkins_elb.dns_name
+}

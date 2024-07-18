cc452dda2638d1d569039230173c62b82b211cdb
lyteabovenyte <lyteabovenyte@gmail.com>
2024-07-19 02:38:17 +0330

    associate route table to the public subnet

--- tf/public_rt.tf
+++ tf/public_rt.tf
@@ -23,6 +23,6 @@ resource "aws_route_table" "public_rt" {
 
 resource "aws_route_table_association" "public" {
     count = var.public_subnets_count
-    subnet_id = element(aws_subnet.public_subnets)
+    subnet_id = element(aws_subnet.public_subnets.*.id, count.index)
     route_table_id = aws_route_table.public_rt
 }
